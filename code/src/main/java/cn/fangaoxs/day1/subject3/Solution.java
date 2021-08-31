e66ab1b1818f098d126a0c687f7246c887d7d9a4
FanGaoXS <954278478@qq.com>
2021-08-31 11:54:26 +0800

    campus@wqk 完成第12天打开

--- code/src/main/java/cn/fangaoxs/day1/subject3/Solution.java
+++ code/src/main/java/cn/fangaoxs/day1/subject3/Solution.java
@@ -7,8 +7,9 @@ import java.util.HashSet;
 class Solution {
     public static void main(String[] args) {
         String s = "a1234588";
-        System.out.println("s.charAt(0) = " + s.charAt(0));
-        System.out.println("lengthOfLongestSubstring(s) = " + lengthOfLongestSubstring(s));
+//        System.out.println("lengthOfLongestSubstring(s) = " + lengthOfLongestSubstring(s));
+        int length = lengthOfLongestSubstring2(s);
+        System.out.println("length = " + length);
     }
     public static int lengthOfLongestSubstring(String s) {
         byte[] bytes = s.getBytes();
@@ -29,5 +30,23 @@ class Solution {
             }
         }
         return maxLength;
+    }
+    public static int lengthOfLongestSubstring2(String s) {
+        if (s.length()==1) return 1;
+        int left = 0;
+        int right = 0;
+        HashSet<Character> set = new HashSet<>();
+        set.add(s.charAt(left));
+        int maxSize = 1;
+        while (left<=right&&right<s.length()){
+            right++;
+            if (!set.add(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            maxSize = Math.max(maxSize, set.size());
+
+        }
+        return maxSize;
     }
 }
--- code/src/main/java/cn/fangaoxs/day12/subject70/Solution.java
+++ code/src/main/java/cn/fangaoxs/day12/subject70/Solution.java
@@ -8,4 +8,38 @@ package cn.fangaoxs.day12.subject70;
  * @Description:
  */
 public class Solution {
+
+    public static int climbStairs(int n) {
+        int[] cache = new int[n];
+        cache[0] = 1;
+        cache[1] = 2;
+        cache[n-1] = climb(n, cache);
+        return cache[n-1];
+    }
+
+    private static int climb(int n,int[] cache){
+        return cache[n-1]+cache[n-2];
+    }
+
+    public static int climbStairs2(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int first = 1;
+        int second = 2;
+        for (int i = 0; i < n-1-1; i++) {
+            int sum = first + second;
+            first = second;
+            second = sum;
+        }
+        return second;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int result = climbStairs(5);
+        int result2 = climbStairs2(n);
+        System.out.println("result = " + result);
+        System.out.println("result2 = " + result2);
+    }
 }
