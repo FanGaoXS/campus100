cc6c4065b88b8befaa9305ce88138e5cde503b82
FanGaoXS <954278478@qq.com>
2021-09-07 19:07:36 +0800

    campus@wqk 完成59和60的打卡

--- code/src/main/java/cn/fangaoxs/day8/subject206/Solution.java
+++ code/src/main/java/cn/fangaoxs/day8/subject206/Solution.java
@@ -1,5 +1,6 @@
 package cn.fangaoxs.day8.subject206;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 
@@ -11,9 +12,11 @@ import java.util.LinkedList;
  * @Description:
  */
 public class Solution {
-    public static ListNode reverseList(ListNode head) {
+
+    //ArrayList，存储int类型
+    public static ListNode reverseList1(ListNode head) {
         if (head==null) return null;
-        LinkedList<Integer> integers = new LinkedList<>();
+        ArrayList<Integer> integers = new ArrayList<>();
         while (head!=null){
             integers.add(head.val);
             head = head.next;
@@ -27,9 +30,69 @@ public class Solution {
         return resultNode.next;
     }
 
+    //ArrayList，存储ListNode类型
+    public static ListNode reverseList2(ListNode head){
+        if (head == null) return null;
+        ArrayList<ListNode> listNodes = new ArrayList<>();
+        while (head!=null){
+            listNodes.add(head);
+            head = head.next;
+        }
+        int size = listNodes.size();
+        ListNode node = listNodes.get(size-1);
+        ListNode tempNode = node;
+        for (int i = listNodes.size()-2; i >= 0 ; i--) {
+            node.next = listNodes.get(i);
+            node = node.next;
+        }
+        node.next = null;
+        return tempNode;
+    }
+
+    // map，map的value是int类型
+    public static ListNode reverseList3(ListNode head){
+        if (head == null) return null;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int i = 0;
+        while (head!=null){
+            map.put(i, head.val);
+            head = head.next;
+            i++;
+        }
+        ListNode node = new ListNode(-1);
+        ListNode tempNode = node;
+        int size = map.size();
+        for (int j = size-1; j >= 0 ; j--) {
+            node.next = new ListNode(map.get(j));
+            node = node.next;
+        }
+        return tempNode.next;
+    }
+
+    //map，map的value是节点类型
+    public static ListNode reverseList4(ListNode head){
+        if (head == null) return null;
+        HashMap<Integer, ListNode> map = new HashMap<>();
+        int i = 0;
+        while (head!=null){
+            map.put(i, head);
+            head = head.next;
+            i++;
+        }
+        int size = map.size();
+        ListNode node = map.get(size-1);
+        ListNode tempNode = node;
+        for (int j = size-2; j >= 0 ; j--) {
+            node.next = map.get(j);
+            node = node.next;
+        }
+        node.next = null;
+        return tempNode;
+    }
+
     public static void main(String[] args) {
-        ListNode head = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5,new ListNode(5))))));
-        ListNode node = reverseList(head);
+        ListNode head = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5)))));
+        ListNode node = reverseList2(head);
         while (node!=null){
             System.out.println("node.val = " + node.val);
             node = node.next;
