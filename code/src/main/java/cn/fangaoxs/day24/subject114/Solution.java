065d9bf5162af3bbf160816396f2739180f50ef4
FanGaoXS <954278478@qq.com>
2021-09-13 23:35:16 +0800

    campus@wqk 完成冲刺073

--- code/src/main/java/cn/fangaoxs/day24/subject114/Solution.java
+++ code/src/main/java/cn/fangaoxs/day24/subject114/Solution.java
@@ -16,23 +16,34 @@ import java.util.List;
  */
 public class Solution {
 
-    public void flatten(TreeNode root) {
-        List<TreeNode> list = new ArrayList<TreeNode>();
-        preorderTraversal(root, list);
-        int size = list.size();
-        for (int i = 1; i < size; i++) {
-            TreeNode prev = list.get(i - 1), curr = list.get(i);
-            prev.left = null;
-            prev.right = curr;
+
+
+    public static void flatten(TreeNode root) {
+        if (root == null) return;
+        ArrayList<TreeNode> nodeList = new ArrayList<>();
+        preorderTraversal(root,nodeList);   //前序遍历root二叉树，并且依次添加进list中
+        TreeNode tempRoot = root;
+        //迭代list，左子树置为null
+        for (int i = 1; i < nodeList.size(); i++) {
+            root.left = null;
+            root.right = new TreeNode(nodeList.get(i).val);
+            root = root.right;
         }
+        root = tempRoot;
+
     }
 
-    public void preorderTraversal(TreeNode root, List<TreeNode> list) {
-        if (root != null) {
-            list.add(root);
-            preorderTraversal(root.left, list);
-            preorderTraversal(root.right, list);
-        }
+    public static void preorderTraversal(TreeNode node,List<TreeNode> nodeList) {
+//        System.out.println("node.val = " + node.val);
+        nodeList.add(node);
+        if (node.left == null && node.right == null) return;
+        if (node.left != null) preorderTraversal(node.left,nodeList);
+        if (node.right != null) preorderTraversal(node.right,nodeList);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)), new TreeNode(5, null, new TreeNode(6)));
+        flatten(root);
     }
 
 }
---
+++ code/src/main/java/cn/fangaoxs/day25/subject148/Solution.java
@@ -0,0 +1,41 @@
+package cn.fangaoxs.day25.subject148;
+
+import cn.fangaoxs.ListNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: 吴青珂
+ * @Date: 2021/09/12/14:53
+ * @Description:
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null) return null;
+        ArrayList<ListNode> nodes = new ArrayList<>();
+        while (head!=null) {
+            nodes.add(head);
+            head = head.next;
+        } //nodes迭代完毕
+        for (int i = 0; i < nodes.size()-1; i++) {
+            for (int j = 0; j < nodes.size()-1-i; j++) {
+                if (nodes.get(j).val>nodes.get(j+1).val) {
+                    ListNode temp = nodes.get(j);
+                    nodes.set(j,nodes.get(j+1));
+                    nodes.set(j+1,temp);
+                }
+            }
+        }
+        ListNode resultNode = new ListNode(-1);
+        ListNode tempNode = resultNode;
+        for (int i = 0; i < nodes.size(); i++) {
+            resultNode.next = nodes.get(i);
+            resultNode = resultNode.next;
+        }
+        resultNode.next = null;
+        return tempNode.next;
+    }
+}
